df9f7b36a81e58895ff246221904be69a70a1ce0
Alberto <a.urech@alumnos.upm.es>
2022-03-26 17:25:20 +0100

    initial commit

---
+++ src/main/java/es/upm/isst/grupo08/trackback/TrackBackApplication.java
@@ -0,0 +1,13 @@
+package es.upm.isst.grupo08.trackback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/upm/isst/grupo08/trackback/TrackBackApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.isst.grupo08.trackback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

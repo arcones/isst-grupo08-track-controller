c5e3fbe6e5f1f5e4cc0cc3b10ea74cde199756c8
Marta Arcones <marta.arcones@gmail.com>
2022-04-02 19:20:37 +0200

    Add documentation

--- src/main/java/es/upm/isst/grupo08/trackback/TrackBackApplication.java
+++ src/main/java/es/upm/isst/grupo08/trackback/TrackBackApplication.java
@@ -1,13 +1,20 @@
 package es.upm.isst.grupo08.trackback;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "Trackermaster API",
+        version = "1.0",
+        description = "API to track and load parcels' tracking information"
+))
 public class TrackBackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrackBackApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TrackBackApplication.class, args);
+    }
 
 }
--- src/main/java/es/upm/isst/grupo08/trackback/controller/TrackController.java
+++ src/main/java/es/upm/isst/grupo08/trackback/controller/TrackController.java
@@ -2,6 +2,10 @@ package es.upm.isst.grupo08.trackback.controller;
 
 import es.upm.isst.grupo08.trackback.model.Parcel;
 import es.upm.isst.grupo08.trackback.repository.ParcelRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +21,7 @@ import java.util.stream.Collectors;
 import static java.util.logging.Level.*;
 
 @RestController
+@Tag(name = "Track", description = "The Trackermaster API")
 public class TrackController {
 
     public static final Logger LOGGER = Logger.getLogger(TrackController.class.getName());
@@ -29,20 +34,20 @@ public class TrackController {
         this.parcelRepository = parcelRepository;
     }
 
+    @Operation(summary = "Login for carriers")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Credentials are correct"), @ApiResponse(responseCode = "404", description = "Credentials doesn't match with any user in the database")})
     @GetMapping("/carriers")
     public ResponseEntity<Void> login(@RequestHeader("User") String user, @RequestHeader("Password") String password) {
         try {
-            boolean correctCredentials = carrierRepository.findAll().stream()
-                    .filter(carrier -> carrier.getName().equalsIgnoreCase(user) && Objects.equals(carrier.getPassword(), password))
-                    .map(anyCarrier -> Boolean.TRUE)
-                    .findAny()
-                    .orElse(Boolean.FALSE);
+            boolean correctCredentials = carrierRepository.findAll().stream().filter(carrier -> carrier.getName().equalsIgnoreCase(user) && Objects.equals(carrier.getPassword(), password)).map(anyCarrier -> Boolean.TRUE).findAny().orElse(Boolean.FALSE);
             return correctCredentials ? new ResponseEntity<>(null, HttpStatus.OK) : new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
+    @Operation(summary = "Bulk upload of parcels for carriers")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Upload was successful"), @ApiResponse(responseCode = "412", description = "There is an error in the carrier IDs provided"), @ApiResponse(responseCode = "406", description = "There is an error in any of the status provided for the parcels"), @ApiResponse(responseCode = "409", description = "There are duplicates within the tracking numbers provided and the system"),})
     @PostMapping("/parcels")
     public ResponseEntity<Void> loadParcels(@RequestBody List<Parcel> inputParcels) {
         try {
@@ -61,12 +66,12 @@ public class TrackController {
         }
     }
 
+    @Operation(summary = "Get parcel information given its tracking number")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Parcel found and data retrieved"), @ApiResponse(responseCode = "404", description = "Parcel not found")})
     @GetMapping("/parcels/{trackingNumber}")
     public ResponseEntity<Parcel> getParcelInfo(@PathVariable String trackingNumber) {
         try {
-            List<Parcel> parcels = parcelRepository.findAll().stream()
-                    .filter(parcelFound -> Objects.equals(parcelFound.getTrackingNumber(), trackingNumber))
-                    .collect(Collectors.toList());
+            List<Parcel> parcels = parcelRepository.findAll().stream().filter(parcelFound -> Objects.equals(parcelFound.getTrackingNumber(), trackingNumber)).collect(Collectors.toList());
             return parcels.size() == 1 ? new ResponseEntity<>(parcels.get(0), HttpStatus.OK) : new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -78,15 +83,9 @@ public class TrackController {
     }
 
     private boolean findDuplicateTrackingNumbersWithCurrents(List<Parcel> inputParcels) {
-        List<String> currentTrackingNumbers = parcelRepository.findAll().stream()
-                .map(Parcel::getTrackingNumber).collect(Collectors.toList());
+        List<String> currentTrackingNumbers = parcelRepository.findAll().stream().map(Parcel::getTrackingNumber).collect(Collectors.toList());
 
-        boolean duplicateTrackingNumberWithCurrents = inputParcels.stream()
-                .map(Parcel::getTrackingNumber)
-                .filter(currentTrackingNumbers::contains)
-                .map(trackingNumber -> Boolean.TRUE)
-                .findAny()
-                .orElse(Boolean.FALSE);
+        boolean duplicateTrackingNumberWithCurrents = inputParcels.stream().map(Parcel::getTrackingNumber).filter(currentTrackingNumbers::contains).map(trackingNumber -> Boolean.TRUE).findAny().orElse(Boolean.FALSE);
 
         if (duplicateTrackingNumberWithCurrents)
             LOGGER.log(WARNING, "There are parcels with a tracking number already present in the database");
@@ -95,12 +94,9 @@ public class TrackController {
     }
 
     private boolean findDuplicateTrackingNumbersAmongInput(List<Parcel> inputParcels) {
-        List<String> currentTrackingNumbersAsList = inputParcels.stream()
-                .map(Parcel::getTrackingNumber).collect(Collectors.toList());
+        List<String> currentTrackingNumbersAsList = inputParcels.stream().map(Parcel::getTrackingNumber).collect(Collectors.toList());
 
-        Set<String> currentTrackingNumbersAsSet = inputParcels.stream()
-                .map(Parcel::getTrackingNumber)
-                .collect(Collectors.toSet());
+        Set<String> currentTrackingNumbersAsSet = inputParcels.stream().map(Parcel::getTrackingNumber).collect(Collectors.toSet());
 
         boolean duplicateTrackingNumbersAmongInputs = currentTrackingNumbersAsList.size() != currentTrackingNumbersAsSet.size();
 
@@ -112,15 +108,9 @@ public class TrackController {
 
 
     private boolean findIncorrectCarriers(List<Parcel> inputParcels) {
-        List<Long> currentCarriers = parcelRepository.findAll().stream()
-                .map(Parcel::getCarrierId).collect(Collectors.toList());
+        List<Long> currentCarriers = parcelRepository.findAll().stream().map(Parcel::getCarrierId).collect(Collectors.toList());
 
-        boolean incorrectCarrier = inputParcels.stream()
-                .map(Parcel::getCarrierId)
-                .filter(carrier -> !currentCarriers.contains(carrier))
-                .map(inputCarrier -> Boolean.TRUE)
-                .findAny()
-                .orElse(Boolean.FALSE);
+        boolean incorrectCarrier = inputParcels.stream().map(Parcel::getCarrierId).filter(carrier -> !currentCarriers.contains(carrier)).map(inputCarrier -> Boolean.TRUE).findAny().orElse(Boolean.FALSE);
 
         if (incorrectCarrier)
             LOGGER.log(WARNING, "There are parcels with a carrier that doesn't exist in the database");
@@ -130,15 +120,9 @@ public class TrackController {
 
     private boolean findIncorrectStatuses(List<Parcel> inputParcels) {
         List<String> correctStatuses = List.of("En tránsito", "Entregado", "Error en la entrega");
-        boolean incorrectStatus = inputParcels.stream()
-                .map(Parcel::getStatus)
-                .filter(status -> !correctStatuses.contains(status))
-                .map(status -> Boolean.TRUE)
-                .findAny()
-                .orElse(Boolean.FALSE);
+        boolean incorrectStatus = inputParcels.stream().map(Parcel::getStatus).filter(status -> !correctStatuses.contains(status)).map(status -> Boolean.TRUE).findAny().orElse(Boolean.FALSE);
 
-        if (incorrectStatus)
-            LOGGER.log(WARNING, "There are parcels with an incorrect status");
+        if (incorrectStatus) LOGGER.log(WARNING, "There are parcels with an incorrect status");
 
         return incorrectStatus;
     }
